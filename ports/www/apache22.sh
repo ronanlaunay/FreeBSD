df521d0489034bd3708f98883962e0302788f8de
Launay <launay.ronan@gmail.com>
2014-07-19 03:10:00 +0200

    Update apache22.sh

--- ports/www/apache22.sh
+++ ports/www/apache22.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 cd /usr/ports/www/apache22
-make -DWITH_OPENSSL -DWITHOUT_IPV6 install clean
+make -DBATCH -DWITH_OPENSSL -DWITHOUT_IPV6 install clean
 echo 'apache2_enable="YES"' >> /etc/rc.conf
